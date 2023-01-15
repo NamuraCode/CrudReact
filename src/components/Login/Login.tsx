6e31ed0f9d6018177c9c32c84c14a9cb5ac1cef8
David Naranjo <dnaranjo0@misena.edu.co>
2023-01-15 01:47:48 -0500

    upload validation login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -7,54 +7,63 @@ function Login() {
 
     const [userName, setUserName] = React.useState("")
     const [userPassword, setUserPassword] = React.useState("")
+    const [userNoFind, setUserNoFind] = React.useState(false)
+
 
     function verifySession(event: React.SyntheticEvent<HTMLFormElement>) {
+        event.preventDefault()
         let userLogin: User
         db.forEach((item) => {
             let salt = bcrypt.genSaltSync(12);
             let passwordEncript = bcrypt.hashSync(userPassword, salt)
-            let passwordComparete = bcrypt.compareSync(userPassword, passwordEncript)
-            if (item.name === userName && passwordComparete) {
+            let passwordComparete = bcrypt.compareSync(item.passWord, passwordEncript)
+            console.log(passwordComparete);
+            if (item.name === userName && passwordComparete && !userNoFind) {
+                console.log(passwordComparete);
                 userLogin = { name: userName, type: item.type }
                 console.log(userLogin);
+                window.sessionStorage.setItem("userLogged", JSON.stringify(userLogin ? userLogin : undefined))
+            }else{
+                setUserNoFind(true)
             }
-            window.sessionStorage.setItem("userLogged", JSON.stringify(userLogin ? userLogin : undefined))
         })
     }
 
     return (
         <div className='main-login'>
-            <div className='login-content' >
-                <h1>Login</h1>
-                <form className='content-login' onSubmit={(e) => { verifySession(e) }}>
-                    <label htmlFor="usernameInput">
-                        Username
-                        <input
-                            id="usernameInput"
-                            value={userName}
-                            onChange={(e) => setUserName(e.target.value)}
-                            type="text"
-                            placeholder="name"
-                            className="input"
-                            required
-                        />
-                    </label>
-                    <br></br>
-                    <label htmlFor="userpasswordInput">
-                        Password
-                        <input
-                            id="userpasswordInput"
-                            value={userPassword}
-                            onChange={(e) => setUserPassword(e.target.value)}
-                            type="password"
-                            placeholder="password"
-                            className="input"
-                            required
-                        />
-                    </label>
-                    <button className='buttton buttton-login' type='submit'>Send</button>
-                </form>
-            </div>
+            { userNoFind ?  <p className='login-content' onClick={()=>{setUserNoFind(false)}}>Try Again</p> :
+                <div className='login-content' >
+                    <h1>Login</h1>
+                    <form className='content-login' onSubmit={(e) => { verifySession(e) }}>
+                        <label htmlFor="usernameInput">
+                            Username
+                            <input
+                                id="usernameInput"
+                                value={userName}
+                                onChange={(e) => setUserName(e.target.value)}
+                                type="text"
+                                placeholder="name"
+                                className="input"
+                                required
+                            />
+                        </label>
+                        <br></br>
+                        <label htmlFor="userpasswordInput">
+                            Password
+                            <input
+                                id="userpasswordInput"
+                                value={userPassword}
+                                onChange={(e) => setUserPassword(e.target.value)}
+                                type="password"
+                                placeholder="password"
+                                className="input"
+                                required
+                            />
+                        </label>
+                        <button className='buttton buttton-login' type='submit'>Send</button>
+                    </form>
+                </div>
+             }
         </div>
     )
 
